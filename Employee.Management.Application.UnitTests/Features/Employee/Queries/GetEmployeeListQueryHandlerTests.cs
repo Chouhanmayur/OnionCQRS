e5556634ee08636d1d3be8f14402fb22947ea42e
chouhanmayur <mayur.chouhan@mindruby.com>
2023-05-31 12:42:30 +0530

    Merge branch 'Dev/GlobalErrorHandling' into env/sit
    # Conflicts:
    #	Employee.Management.Application/obj/Debug/net6.0/Employee.Management.Application.csproj.AssemblyReference.cache
    #	Employee.Management.Infrastructure/obj/Debug/net6.0/Employee.Management.Infrastructure.csproj.AssemblyReference.cache
    #	Employee.Management.Persistence/obj/Debug/net6.0/Employee.Management.Persistence.csproj.AssemblyReference.cache
    #	EmployeeManagement.API/obj/Debug/net6.0/EmployeeManagement.API.assets.cache
    #	EmployeeManagement.API/obj/Debug/net6.0/EmployeeManagement.API.csproj.AssemblyReference.cache

---
+++ Employee.Management.Application.UnitTests/Features/Employee/Queries/GetEmployeeListQueryHandlerTests.cs
@@ -0,0 +1,50 @@
+﻿using AutoMapper;
+using Employee.Management.Application.Contracts.Logging;
+using Employee.Management.Application.Contracts.Persistence;
+using Employee.Management.Application.Features.Employee.Querys.GetAllEmployees;
+using Employee.Management.Application.MappingProfiles;
+using Employee.Management.Application.UnitTests.Mocks;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Shouldly;
+
+namespace Employee.Management.Application.UnitTests.Features.Employee.Queries
+{
+    public class GetEmployeeListQueryHandlerTests
+    {
+        private readonly Mock<IEmployeeRepository> _mockRepo;
+        private IMapper _mapper;
+        private Mock<IAppLogger<GetEmployeeQueryHandler>> _mockAppLogger;
+
+        public GetEmployeeListQueryHandlerTests()
+        {
+            _mockRepo = MockEmployeeRepository.GetEmployeeTypeRepository();
+
+            var mapperConfig = new MapperConfiguration(c =>
+             {
+                 c.AddProfile<EmployeeProfile>();
+                 });
+
+            _mapper = mapperConfig.CreateMapper();
+            _mockAppLogger = new Mock<IAppLogger<GetEmployeeQueryHandler>>();
+        }
+
+
+        [Fact]
+        public async Task GetEmployeeListTest()
+        {
+            var handler = new GetEmployeeQueryHandler (_mapper, _mockRepo.Object, 
+                _mockAppLogger.Object);
+
+            var result = await handler.Handle(new GetEmployeeQuery(), CancellationToken.None);
+
+            result.ShouldBeOfType<List<EmployeeDTO>>();
+            result.Count.ShouldBe(1);
+
+        }
+    }
+}
---
+++ Employee.Management.Application.UnitTests/Mocks/MockEmployeeRepository.cs
@@ -0,0 +1,45 @@
+﻿using Employee.Management.Application.Contracts.Persistence;
+using Employee.Management.Domain;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employee.Management.Application.UnitTests.Mocks
+{
+    public class MockEmployeeRepository
+    {
+        public static Mock<IEmployeeRepository> GetEmployeeTypeRepository()
+        {
+            var Employees = new List<Domain.Employee>
+            { new Domain.Employee {
+           
+            City = "Indore",
+            Name = "unit Test",
+            Department = "CS",
+            Gender = "Male"
+            }};
+
+            var mockRepo = new Mock<IEmployeeRepository>();
+            mockRepo.Setup(r => r.GetAsync()).ReturnsAsync(Employees);
+            //mockRepo.Setup(r => r.CreateAsync(It.IsAny<Domain.Employee>()))
+            //    .Returns((Domain.Employee employee) =>
+            //    {
+            //        Employees.Add(employee);
+            //        return Task.CompletedTask;
+            //    });
+
+            mockRepo.Setup(r => r.CreateAsync(It.IsAny<Domain.Employee>()))
+     .Returns((Domain.Employee employee) =>
+     {
+         Employees.Add(employee);
+         return Task.FromResult(employee); // Return Task<Employee> instead of Task
+     });
+
+            return mockRepo;
+        }
+
+    }
+}
